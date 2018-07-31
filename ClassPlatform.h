48b4ae39dea96846ca63b8bbabc541de74208ffc
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-07-31 22:01:17 +0300

    Update ClassPlatform.h

--- ClassPlatform.h
+++ ClassPlatform.h
@@ -1,21 +1,32 @@
 #pragma once
 #ifndef ClassPlatform
+#include "GameSetUp.h"
 
 class Platform
 {
 private:
 	char platformSign;
 	int platformWidth;
+	int x, y;
+	int score;
 public:
 	Platform()
 	{
 		platformSign = char(219);
 		platformWidth = 5;
+		score = 0;
 	}
-	~Platform() {}
+	void SetCoordinates(int X, int Y, PlatformDirection &dir);
 	void BuildingPlatform(int);
-	void EnemyPlatformMoving();
-	void PlatformTouchingTheWall();
+	void EnemyPlatformMoveLogic();
+	void TouchingTheWall(PlatformDirection &dir);
+	void GetKeyboardCommands();
+	void MovementDescription(PlatformDirection movement);
+	friend class Map;
+	friend class Ball;
 };
+
+Platform enemyPlatform;
+Platform playerPlatform;
 
 #endif // !ClassPlatform
