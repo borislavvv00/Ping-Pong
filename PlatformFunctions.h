c02cef1ee6c8d6277fe19fbf8260e0a528da6631
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-07-31 22:03:19 +0300

    Update PlatformFunctions.h

--- PlatformFunctions.h
+++ PlatformFunctions.h
@@ -1,19 +1,22 @@
 #pragma once
 #ifndef PlatformFunctions
-#include <iostream>
-#include <Windows.h>
 #include "ClassPlatform.h"
-
-using namespace std;
 
 void Platform::BuildingPlatform(int color)
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 	for (int i = 0; i < platformWidth; i++)
 	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 		cout << platformSign;
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 	}
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+
+void Platform::SetCoordinates(int X, int Y, PlatformDirection &dir)
+{
+	x = X;
+	y = Y;
+	dir = PlatformDirection::stop;
 }
 
 #endif // !PlatformFunctions
