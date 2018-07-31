af0ca9b9885008e4f3b0d528726e73196a8d75f8
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-07-31 22:00:29 +0300

    Update ClassBall.h

--- ClassBall.h
+++ ClassBall.h
@@ -1,19 +1,27 @@
 #pragma once
 #ifndef ClassBall
+#include "GameSetUp.h"
 
 class Ball 
 {
+private:
+	int x, y;
+	void ChooseDirection(int dir1, int dir2);//dir = direction
 public:
 	char ballSign;
 	Ball()
 	{
 		ballSign = '.';
 	}
-	~Ball() {}
-	void BallTouchingPlayerPlatform();
-	void BallTouchingEnemyPlatform();
-	void BallTouchingTheWall();
-	void BallReset();
+	void TouchingPlayerPlatform();
+	void TouchingEnemyPlatform();
+	void TouchingTheWall();
+	void SetCoordinates();
+	void MovementDescription();
+	friend class Map;
+	friend class Platform;
 };
+
+Ball ball;
 
 #endif // !ClassBall
