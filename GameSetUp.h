c2cf38aefddba8128fb9f41c5cb5486016ae8482
Borislav Kalinski <borislav_kalinski@abv.bg>
2018-08-01 19:21:13 +0300

    Update GameSetUp.h

--- GameSetUp.h
+++ GameSetUp.h
@@ -3,13 +3,14 @@
 #include <iostream>
 #include <Windows.h>
 #include <conio.h>
+#include <bitset>
 #define WIDTH 40
 #define HIGH 20
 
 using namespace std;
 
 bool isGameOver = false;
-bool ballTrajectory;//0-hit by enemy; 1-hit by player
+bitset<1> ballTrajectory;//0-hit by enemy; 1-hit by player
 
 enum class BallDirection
 {
